038edbf221c82f6ff53b8f2b9c7b346d100f3382
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-06-17 02:22:45 +0600

    init

---
+++ sort_linklist.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val) // constructor
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void print_linkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int val;
+    Node *head = NULL;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, val);
+        }
+    }
+
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val < j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
+    print_linkedList(head);
+
+    return 0;
+}
