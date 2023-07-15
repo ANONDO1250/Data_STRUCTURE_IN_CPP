0f93da345781b878127455ebe9891252e3829b0c
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-07-15 21:33:39 +0600

    init

--- Binaray Tree/binary_search_implement.cpp
+++ Binaray Tree/binary_search_implement.cpp
@@ -18,6 +18,16 @@ public:
 
 int main()
 {
+    Node *root = new Node(10);
+    Node *a = new Node(10);
+    Node *b = new Node(10);
+    Node *c = new Node(10);
+    Node *d = new Node(10);
+    Node *e = new Node(10);
+    Node *f = new Node(10);
+    Node *g = new Node(10);
+    Node *h = new Node(10);
+    Node *i = new Node(10);
 
     return 0;
 }
