a5bc2e34a7d741c13de442f2259621e0592fe4da
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-07-15 22:21:27 +0600

    init

--- Binaray Tree/inORDER_traverse.cpp
+++ Binaray Tree/inORDER_traverse.cpp
@@ -15,19 +15,29 @@ public:
         this->right = NULL;
     }
 };
-
+void IN_Order(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+    IN_Order(root->left);
+    cout << root->val << " ";
+    IN_Order(root->right);
+}
 int main()
 {
     Node *root = new Node(10);
-    Node *a = new Node(10);
-    Node *b = new Node(10);
-    Node *c = new Node(10);
-    Node *d = new Node(10);
-    Node *e = new Node(10);
-    Node *f = new Node(10);
-    Node *g = new Node(10);
-    Node *h = new Node(10);
-    Node *i = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
 
     root->left = a;
     root->right = b;
@@ -44,9 +54,12 @@ int main()
 
     h->right = i;
 
+    // call
+
+    IN_Order(root);
+
+    return 0;
     // preorder->Root, left, right;
     // inOrder->Left, root, right;
     // postOrder->left, right, root;
-
-    return 0;
 }
