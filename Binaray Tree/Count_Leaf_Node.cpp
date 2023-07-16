c3559a7639c618bd207ee9ea7525b6af6ff3f1ea
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-07-16 16:16:53 +0600

    init

---
+++ Binaray Tree/Count_Leaf_Node.cpp
@@ -0,0 +1,91 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        // 1.ber kore anlam
+        Node *f = q.front();
+        q.pop();
+
+        // 2.joto kaj ase ekhane
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        f->left = myLeft;
+        f->right = myRight;
+
+        // 3.children gulo k push koro
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+            q.push(f->right);
+    }
+    return root;
+}
+int count_leaf(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        int l = count_leaf(root->left);
+        int r = count_leaf(root->right);
+        return l + r;
+    }
+}
+
+int main()
+{
+    Node *root = input_tree();
+    cout << count_leaf(root) << endl;
+
+    return 0;
+}
