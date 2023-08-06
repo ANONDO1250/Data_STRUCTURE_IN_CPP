f8863d69f619db6c1853f47ded3dad7f3c68292e
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-08-06 14:26:53 +0600

    init

---
+++ Binaray Tree/BST/search_in_BST.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        // 1.ber kore anlam
+        Node *f = q.front();
+        q.pop();
+
+        // 2.joto kaj ase ekhane
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        f->left = myLeft;
+        f->right = myRight;
+
+        // 3.children gulo k push koro
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+            q.push(f->right);
+    }
+    return root;
+}
+void level_Order(Node *root)
+{
+    queue<Node *> q;
+
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1.ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // 2.jabotiyo ja kaj ase
+        cout << f->val << " ";
+
+        // 3.children gulok ana
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+            q.push(f->right);
+    }
+}
+bool search(Node *root, int x)
+{
+
+    if (root == NULL)
+        return false;
+    if (root->val == x)
+        return true;
+
+    if (x < root->val)
+    {
+        return search(root->left, x);
+    }
+    else
+    {
+        return search(root->right, x);
+    }
+}
+int main()
+{
+
+    Node *root = input_tree();
+    if (search(root, 6))
+        cout << " Yes , Found" << endl;
+    else
+        cout << "NOt found";
+    return 0;
+}
